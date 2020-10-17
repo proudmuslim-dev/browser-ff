2fd0ed428b32c12a79a597b965d398fdd0c2655f
EnderDev <xenderpugx@gmail.com>
2020-10-17 23:12:03 +0100

    test

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -3,7 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_VENDOR=Mozilla
+MOZ_APP_VENDOR=Dot
 MOZ_UPDATER=1
 
 if test "$OS_ARCH" = "WINNT"; then
@@ -34,7 +34,7 @@ BROWSER_CHROME_URL=chrome://browser/content/browser.xhtml
 # For mozilla-beta, mozilla-release, or mozilla-central repositories, use
 # "unofficial" branding.
 # For the mozilla-aurora repository, use "aurora".
-MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
+MOZ_BRANDING_DIRECTORY=browser/branding/nightly
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
 MOZ_APP_ID={ec8030f7-c20a-464f-9b0e-13a3a9e97384}
 
